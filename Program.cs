b6c1a8ef32e65e0d6add21985d2e737768ada98f
Tarchom Tarik <tarachom@i.ua>
2020-01-22 14:02:02 +0200

    up

--- Program.cs
+++ Program.cs
@@ -13,74 +13,74 @@ namespace ErlangSatelite
 	{
 		static void Main(string[] args)
 		{
-            Thread threadListener = new Thread(new ThreadStart(GeneralWorker));
-            threadListener.IsBackground = true;
-            threadListener.Start();
+			Thread threadListener = new Thread(new ThreadStart(GeneralWorker));
+			threadListener.IsBackground = true;
+			threadListener.Start();
 
-            Console.ReadLine();
-        }
+			Console.ReadLine();
+		}
 
-        static void GeneralWorker()
-        {
+		static void GeneralWorker()
+		{
 
-            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5555);
+			IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5555);
 
-            Console.WriteLine(localEndPoint.AddressFamily.ToString());
+			Console.WriteLine(localEndPoint.AddressFamily.ToString());
 
-            Socket soketWork = new Socket(localEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+			Socket soketWork = new Socket(localEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
-            Console.WriteLine("[" + DateTime.Now.ToString() + "] --> Listener [0][Connect] - <OK>");
+			Console.WriteLine("[" + DateTime.Now.ToString() + "] --> Listener [0][Connect] - <OK>");
 
-            soketWork.Bind(localEndPoint);
-            soketWork.Listen(100);
+			soketWork.Bind(localEndPoint);
+			soketWork.Listen(100);
 
-            Console.WriteLine("[" + DateTime.Now.ToString() + "] --> Listener [0][Listen] - <OK>");
+			Console.WriteLine("[" + DateTime.Now.ToString() + "] --> Listener [0][Listen] - <OK>");
 
-            while (true)
-            {
-                Console.WriteLine("[" + DateTime.Now.ToString() + "] --> Listener [0][Accept] - <Accept>");
+			while (true)
+			{
+				Console.WriteLine("[" + DateTime.Now.ToString() + "] --> Listener [0][Accept] - <Accept>");
 
-                try
-                {
-                    //Очікування підключення
-                    Socket soketAccept = soketWork.Accept();
-                    soketAccept.ReceiveTimeout = 5000;
+				try
+				{
+					//Очікування підключення
+					Socket soketAccept = soketWork.Accept();
+					soketAccept.ReceiveTimeout = 5000;
 
-                    Console.WriteLine("[" + DateTime.Now.ToString() + "] --> Listener [0][Accept][" + soketAccept.RemoteEndPoint.ToString() + "] - <OK>");
+					Console.WriteLine("[" + DateTime.Now.ToString() + "] --> Listener [0][Accept][" + soketAccept.RemoteEndPoint.ToString() + "] - <OK>");
 
-                    //буфер для сокета
-                    Byte[] buffer = new Byte[1024];
+					//буфер для сокета
+					Byte[] buffer = new Byte[1024];
 
-                    int receiveByte = 0;
-                    string receiveXmlText = "";
+					int receiveByte = 0;
+					string receiveXmlText = "";
 
-                    //Зчитую дані
-                    do
-                    {
-                        Console.WriteLine(soketAccept.Available);
-                        receiveByte = soketAccept.Receive(buffer);
-                        Console.WriteLine(soketAccept.Available);
-                        receiveXmlText += Encoding.GetEncoding(1251).GetString(buffer, 0, receiveByte);
-                    }
-                    while (soketAccept.Available > 0);
+					//Зчитую дані
+					do
+					{
+						Console.WriteLine(soketAccept.Available);
+						receiveByte = soketAccept.Receive(buffer);
+						Console.WriteLine(soketAccept.Available);
+						receiveXmlText += Encoding.GetEncoding(1251).GetString(buffer, 0, receiveByte);
+					}
+					while (soketAccept.Available > 0);
 
-                    Console.WriteLine("Receive: " + receiveXmlText);
+					Console.WriteLine("Receive: " + receiveXmlText);
 
-                    string otvet = receiveXmlText + "<br/><h1>OK<h1/>";
+					string otvet = receiveXmlText + "<br/><h1>OK<h1/>";
 
-                    Console.WriteLine("Send: " + otvet);
-                    soketAccept.Send(Encoding.GetEncoding(1251).GetBytes(Convert.ToString(otvet)));
+					Console.WriteLine("Send: " + otvet);
+					soketAccept.Send(Encoding.GetEncoding(1251).GetBytes(Convert.ToString(otvet)));
 
-                    Console.WriteLine("Close");
-                    soketAccept.Close();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("[" + DateTime.Now.ToString() + "] --> Listener [0][Exception - <" + ex.Message + ">");
-                }
-            }
+					Console.WriteLine("Close");
+					soketAccept.Close();
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine("[" + DateTime.Now.ToString() + "] --> Listener [0][Exception - <" + ex.Message + ">");
+				}
+			}
 
-            //Console.WriteLine("[" + DateTime.Now.ToString() + "] <-- Listener [0] - <Close>");
-        }
-    }
+			//Console.WriteLine("[" + DateTime.Now.ToString() + "] <-- Listener [0] - <Close>");
+		}
+	}
 }
